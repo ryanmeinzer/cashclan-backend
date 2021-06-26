6d51cde49e07b427471e24118529fae689808566
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-06-26 13:17:35 -0700

    seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,25 +9,12 @@
 require 'faker'
 
 Member.destroy_all
-Seller.destroy_all
-Buyer.destroy_all
+Transaction.destroy_all
 
 10.times do
-    Member.create(name: Faker::Name.unique.name, phone: Faker::Number.number(digits: 10), venmo: Faker::Internet.username, selling: rand(2) == 1, buying: rand(2) == 1)
+    Member.create(name: Faker::Name.unique.name, phone: Faker::Number.number(digits: 10), venmo: Faker::Internet.username, selling: rand(2) == 1, buying: rand(2) == 1, amount: Faker::Number.between(from: 1, to: 100), location: '37.794374248633815, -122.400108679331')
 end
 
-Seller.create(member_id: Member.all.first.id, location: '37.794374248633815, -122.400108679331', amount: Faker::Number.between(from: 1, to: 100))
-
-Seller.create(member_id: Member.all.second.id, location: '37.794374248633815, -122.400108679331', amount: Faker::Number.between(from: 1, to: 100))
-
-Seller.create(member_id: Member.all.third.id, location: '37.794374248633815, -122.400108679331', amount: Faker::Number.between(from: 1, to: 100))
-
-Buyer.create(member_id: Member.all[9].id, location: '37.794374248633815, -122.400108679331', amount: Faker::Number.between(from: 1, to: 100))
-
-Buyer.create(member_id: Member.all[8].id, location: '37.794374248633815, -122.400108679331', amount: Faker::Number.between(from: 1, to: 100))
-
-Buyer.create(member_id: Member.all[7].id, location: '37.794374248633815, -122.400108679331', amount: Faker::Number.between(from: 1, to: 100))
-
-20.times do
-    Transaction.create(seller_id: Seller.all.sample.id, buyer_id: Buyer.all.sample.id, seller_confirmed: rand(2) == 1, buyer_confirmed: rand(2) == 1, status: 'pending', amount: Faker::Number.between(from: 1, to: 100), location: '37.794374248633815, -122.400108679331')
+30.times do
+    Transaction.create(seller_id: Member.all.sample.id, buyer_id: Member.all.sample.id, seller_confirmed: rand(2) == 1, buyer_confirmed: rand(2) == 1, status: 'pending', amount: Faker::Number.between(from: 1, to: 100), location: '37.794374248633815, -122.400108679331')
 end
