d18161f2226bcfffc31939e9a3b66ac9f9a9af33
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-06-25 19:33:35 -0700

    included create update and params for getter, giver and transactions

--- app/controllers/givers_controller.rb
+++ app/controllers/givers_controller.rb
@@ -1,2 +1,25 @@
 class GiversController < ApplicationController
+
+    def index
+        givers = Giver.all 
+        render json: givers
+    end
+    
+    def create
+        giver = Giver.create(giver_params)
+        # render json: giver
+    end
+
+    def update
+        giver = Giver.find(params[:id])
+        giver.update(giver_params)
+        # render json: giver
+    end
+
+    private
+
+    def giver_params
+        params.require(:giver).permit(:name, :phone, :venmo, :location, :amount)
+    end
+
 end
--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,25 @@
 class TransactionsController < ApplicationController
+
+    def index
+        transactions = Transaction.all 
+        render json: transactions
+    end
+
+    def create
+        transaction = Transaction.create(transaction_params)
+        # render json: transaction
+    end
+
+    def update
+        transaction = Transaction.find(params[:id])
+        transaction.update(transaction_params)
+        # render json: transaction
+    end
+
+    private
+
+    def transaction_params
+        params.require(:transaction).permit(:giver_id, :getter_id, :giver_confirmed, :getter_confirmed, :status, :amount, :location)
+    end
+
 end
