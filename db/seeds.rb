6c0bd8e74fa0d58118beea97e10fe63b6a6b27c7
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2022-01-01 17:28:53 -0800

    cleaned up seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require 'faker'
 Member.destroy_all
 Transaction.destroy_all
 
-10.times do
+30.times do
     Member.create(name: Faker::Name.unique.name, phone: Faker::Number.number(digits: 10), venmo: Faker::Internet.username, active: rand(2) == 1, mode: ['buying', 'selling'].sample, amount: Faker::Number.between(from: 10, to: 300), premium: Faker::Number.between(from: 1, to: 10), location: ["McTeague's Saloon", 'Teeth', 'Noc Noc'].sample)
 end
 
