32845629c01c2775439de028cb25d2dcfc387ced
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2022-02-07 22:37:54 -0800

    changed from purchases and sales to buys and sells

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,4 @@
 class Member < ApplicationRecord
-    has_many :purchases, :class_name => 'Transaction', :foreign_key => 'buyer_id'
-    has_many :sales, :class_name => 'Transaction', :foreign_key => 'seller_id'
+    has_many :buys, :class_name => 'Transaction', :foreign_key => 'buyer_id'
+    has_many :sells, :class_name => 'Transaction', :foreign_key => 'seller_id'
 end
