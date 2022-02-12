d728fa3854d628747189095e9eed91ee48d0d09c
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2022-02-12 15:48:12 -0800

    testing first_or_create

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -12,27 +12,29 @@ class TransactionsController < ApplicationController
         # find or create where the user's last transaction isn't completed and their match is equal to their params match
 
         # the below works, but doesn't update the db as it finds the current transaction
-        # transaction = Transaction.where(:buyer_id => params[:buyer_id], :seller_id => params[:seller_id], :amount => params[:amount]).first_or_create(transaction_params)
+        transaction = Transaction.where(:buyer_id => params[:buyer_id], :seller_id => params[:seller_id], :amount => params[:amount]).first_or_create(transaction_params)
 
-        transaction = Transaction.where(:buyer_id => params[:buyer_id], :seller_id => params[:seller_id], :amount => params[:amount]).find_or_create do |transaction|
-            transaction.amount: params[:amount]
-            transaction.premium: params[:premium]
-            transaction.cost: params[:cost]
-            transaction.profit: params[:profit]
-            transaction.savings: params[:savings]
-            transaction.buyer_offer_amount: params[:buyer_offer_amount]
-            transaction.buyer_offer_premium: params[:buyer_offer_premium]
-            transaction.seller_offer_amount: params[:seller_offer_amount]
-            transaction.seller_offer_premium: params[:seller_offer_premium]
-            transaction.seller_id: params[:seller_id]
-            transaction.buyer_id: params[:buyer_id]
-            # seller_confirmed: params[:seller_confirmed] ? params[:seller_confirmed] : false
-            # buyer_confirmed: params[:buyer_confirmed] ? params[:buyer_confirmed] : false
-            # status: seller_confirmed && buyer_confirmed ? 'complete' : 'pending'
-            transaction.seller_confirmed: params[:seller_confirmed]
-            transaction.buyer_confirmed: params[:buyer_confirmed]
-            transaction.status: params[:status]
-            transaction.location: params[:location]
+        # transaction.update_attribute((:buyer_confirmed, params[:buyer_confirmed])
+
+        # transaction = Transaction.where(:buyer_id => params[:buyer_id], :seller_id => params[:seller_id], :amount => params[:amount]).find_or_create do |transaction|
+        #     transaction.amount: params[:amount]
+        #     transaction.premium: params[:premium]
+        #     transaction.cost: params[:cost]
+        #     transaction.profit: params[:profit]
+        #     transaction.savings: params[:savings]
+        #     transaction.buyer_offer_amount: params[:buyer_offer_amount]
+        #     transaction.buyer_offer_premium: params[:buyer_offer_premium]
+        #     transaction.seller_offer_amount: params[:seller_offer_amount]
+        #     transaction.seller_offer_premium: params[:seller_offer_premium]
+        #     transaction.seller_id: params[:seller_id]
+        #     transaction.buyer_id: params[:buyer_id]
+        #     # seller_confirmed: params[:seller_confirmed] ? params[:seller_confirmed] : false
+        #     # buyer_confirmed: params[:buyer_confirmed] ? params[:buyer_confirmed] : false
+        #     # status: seller_confirmed && buyer_confirmed ? 'complete' : 'pending'
+        #     transaction.seller_confirmed: params[:seller_confirmed]
+        #     transaction.buyer_confirmed: params[:buyer_confirmed]
+        #     transaction.status: params[:status]
+        #     transaction.location: params[:location]
 
         # @member_last_transaction = Transaction.find(params[:member_id]).transactions.last
         # @transaction = Transaction.where(member_id: params[:seller_id || :buyer_id])
