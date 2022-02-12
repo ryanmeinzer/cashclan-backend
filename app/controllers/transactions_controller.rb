f8b171d47c8057756efb20f8cc60e231bc6c5a82
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2022-02-12 15:07:23 -0800

    reverting

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -7,42 +7,32 @@ class TransactionsController < ApplicationController
     
     # ToDo - find or create a transaction (vs. just create)
     def create
-        # transaction = Transaction.create(transaction_params)
+        transaction = Transaction.create(transaction_params)
 
         # find or create where the user's last transaction isn't completed and their match is equal to their params match
 
+        # transaction = Transaction.where(:buyer_id => params[:buyer_id], :seller_id => params[:seller_id], :amount => params[:amount]).first_or_create(transaction_params)
 
-        transaction = Transaction.where(:buyer_id => params[:buyer_id], :seller_id => params[:seller_id], :amount => params[:amount]).first_or_create do |transaction|
-
-            amount: params[:amount]
-            premium: params[:premium]
-            cost: params[:cost]
-            profit: params[:profit]
-            savings: params[:savings]
-            buyer_offer_amount: params[:buyer_offer_amount]
-            buyer_offer_premium: params[:buyer_offer_premium]
-            seller_offer_amount: params[:seller_offer_amount]
-            seller_offer_premium: params[:seller_offer_premium]
-            seller_id: params[:seller_id]
-            buyer_id: params[:buyer_id]
-            seller_confirmed: params[:seller_confirmed] ? params[:seller_confirmed] : false
-            buyer_confirmed: params[:buyer_confirmed] ? params[:buyer_confirmed] : false
-            status: seller_confirmed && buyer_confirmed ? 'complete' : 'pending'
-            location: params[:location]
+        # transaction = Transaction.where(:buyer_id => params[:buyer_id], :seller_id => params[:seller_id], :amount => params[:amount]).first_or_create do |transaction|
+        #     amount: params[:amount]
+        #     premium: params[:premium]
+        #     cost: params[:cost]
+        #     profit: params[:profit]
+        #     savings: params[:savings]
+        #     buyer_offer_amount: params[:buyer_offer_amount]
+        #     buyer_offer_premium: params[:buyer_offer_premium]
+        #     seller_offer_amount: params[:seller_offer_amount]
+        #     seller_offer_premium: params[:seller_offer_premium]
+        #     seller_id: params[:seller_id]
+        #     buyer_id: params[:buyer_id]
+        #     seller_confirmed: params[:seller_confirmed] ? params[:seller_confirmed] : false
+        #     buyer_confirmed: params[:buyer_confirmed] ? params[:buyer_confirmed] : false
+        #     status: seller_confirmed && buyer_confirmed ? 'complete' : 'pending'
+        #     location: params[:location]
 
         # @member_last_transaction = Transaction.find(params[:member_id]).transactions.last
         # @transaction = Transaction.where(member_id: params[:seller_id || :buyer_id])
         # transaction = Transaction.find_or_create_by(params[:id]) do |member|
-
-            # member.name = params[:name]
-            # member.email = params[:email]
-            # member.image = params[:image]
-            # member.phone = params[:phone]
-            # member.active = params[:active]
-            # member.mode = params[:mode]
-            # member.amount = params[:amount]
-            # member.premium = params[:premium]
-            # member.location = params[:location]
         end
 
 
