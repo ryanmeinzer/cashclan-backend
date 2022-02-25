258328cbad909257e8bd29ec1617a8e691668f2a
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2022-02-24 21:48:59 -0800

    updating transaction update to search by params rather than id

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -6,8 +6,7 @@ class TransactionsController < ApplicationController
     end
     
     def create
-        # ToDo - ensure members only ever have a single pending transaction
-
+        # ensure new transaction is created only if it's a new transaction (also handled on FE)
         pending_transaction = Transaction.find_by(buyer_id: params[:buyer_id], seller_id: params[:seller_id], status: 'pending', amount: params[:amount], premium: params[:premium], location: params[:location])
         if pending_transaction
             render json: pending_transaction
@@ -15,34 +14,17 @@ class TransactionsController < ApplicationController
             transaction = Transaction.create(transaction_params)
             render json: transaction
         end
-
-        # Transaction.find_or_create_by(transaction_params)
-        # transaction = Transaction.find_by(transaction_params)
-        # render json: transaction
-
-        # transaction = Transaction.find(params[:id])
-        # if transaction.exists?
-        #     render json: transaction
-        # else
-        #     transaction = Transaction.create(transaction_params)
-        #     render json: transaction
-        # end
-
-        # if Transaction.where(:buyer_id => params[:buyer_id], :seller_id => params[:seller_id], :amount => params[:amount], status: 'pending').exists?
-        #     transaction = Transaction.find_by(buyer_id: params[:buyer_id], seller_id: params[:seller_id], amount: params[:amount])
-        #     transaction.update(seller_confirmed: true, buyer_confirmed: true, status: 'complete')
-        #     # seller = Member.find_by(seller_id: params[:seller_id])
-        #     # seller.update(active: false)
-        #     # buyer = Member.find_by(buyer_id: params[:buyer_id])
-        #     # buyer.update(active: false)
-        # else
-            # Transaction.create(transaction_params)
-        # end
     end
 
     def update
-        transaction = Transaction.find(params[:id])
+
+        transaction = Transaction.find_by(buyer_id: params[:buyer_id], seller_id: params[:seller_id], status: 'pending', amount: params[:amount], premium: params[:premium], location: params[:location])
         transaction.update(transaction_params)
+
+        # ToDo - swap above with below once assuredly passing (pending or new) transaction id
+        # transaction = Transaction.find(params[:id])
+        # transaction.update(transaction_params)
+
         buyer = Member.find(params[:buyer_id])
         seller = Member.find(params[:seller_id])
         buyer.update(active: false, mode: '', amount: 10, premium: 1, location: '')
