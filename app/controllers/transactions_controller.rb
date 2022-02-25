2ec6454e1d8e41b9cc8914aef9c1770ac46a6d6b
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2022-02-25 13:41:34 -0800

    updating per ruby

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -17,10 +17,10 @@ class TransactionsController < ApplicationController
         #     transaction = Transaction.create(transaction_params)
         #     render json: transaction
         # end
-        # if !pending_identical_transaction and !pending_transaction
+        if !pending_identical_transaction.present? and !pending_transaction.present?
             transaction = Transaction.create(transaction_params)
             render json: transaction
-        # end
+        end
     end
 
     def update
