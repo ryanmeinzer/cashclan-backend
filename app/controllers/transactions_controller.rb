4ef8cd41698c70e1237b06d07851a61e3e1bc4d7
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2022-03-04 14:27:06 -0800

    rescuing sms exceptions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -27,24 +27,31 @@ class TransactionsController < ApplicationController
             twilio_number = ENV['TWILIO_NUMBER']
             # test_number = ENV['TEST_NUMBER']
             
-            # send buyer an SMS reminder if they have a phone
-            if buyer_phone.present?
-                client = Twilio::REST::Client.new(account_sid, auth_token)
-                    client.messages.create(
-                        body: "You've been matched at #{params[:location]} with #{seller_name}! Meet now at the ATM. For transaction specifics (or to cancel), login at https://cashclan.com.",
-                        from: twilio_number,
-                        to: "+#{buyer_phone}"
-                    )
-                    puts message.status
+            begin
+                # send buyer an SMS reminder if they have a phone
+                if buyer_phone.present?
+                    client = Twilio::REST::Client.new(account_sid, auth_token)
+                        client.messages.create(
+                            body: "You've been matched at #{params[:location]} with #{seller_name}! Meet now at the ATM. For transaction specifics (or to cancel), login at https://cashclan.com.",
+                            from: twilio_number,
+                            to: "+#{buyer_phone}"
+                        )
+                end
+            rescue => e
+                puts e
             end
-            # send seller an SMS reminder if they have a phone
-            if seller_phone.present?
-                client.messages.create(
-                body: "You've been matched at #{params[:location]} with #{buyer_name}! Meet now at the ATM. For transaction specifics (or to cancel), login at https://cashclan.com.",
-                from: twilio_number,
-                to: "+#{seller_phone}"
-                )
-                puts message.status
+            
+            begin
+                # send seller an SMS reminder if they have a phone
+                if seller_phone.present?
+                    client.messages.create(
+                    body: "You've been matched at #{params[:location]} with #{buyer_name}! Meet now at the ATM. For transaction specifics (or to cancel), login at https://cashclan.com.",
+                    from: twilio_number,
+                    to: "+#{seller_phone}"
+                    )
+                end
+            rescue => e
+                puts e
             end
 
             render json: transaction
