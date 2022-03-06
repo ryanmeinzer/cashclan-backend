99620ee1650e170491a04f89f8ee30527f1fccb5
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2022-03-05 21:08:42 -0800

    showing phone for only current member

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -56,9 +56,15 @@ class MembersController < ApplicationController
     # ToDo - only show phone if/for current member
     def show
         member = Member.find(params[:id])
-        render json: member.to_json({
-            except: [:created_at, :updated_at, :googleId, :email]
-        })
+        if params[:name].present?
+            render json: member.to_json({
+                except: [:created_at, :updated_at, :googleId, :email]
+            })
+        else
+            render json: member.to_json({
+                except: [:created_at, :updated_at, :googleId, :email, :phone]
+            })
+        end
     end
 
     private
