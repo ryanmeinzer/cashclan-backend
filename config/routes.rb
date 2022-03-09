4560f974742caaae08697a061f4f1f9f4a535c1b
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2022-03-08 22:07:01 -0800

    removing routes entirely as test

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   direct :transactions do
     "https://cashclan.com"
   end
-  resources :members
-  resources :transactions
+  # resources :members
+  # resources :transactions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
