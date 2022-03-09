b5beef0daf9ac1abbed6c5626d2c2958ec76c15a
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2022-03-09 14:19:12 -0800

    reverting with trying decodeURI on FE

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # root to: "application#cookie"
+  root to: "application#cookie"
   resources :members
   resources :transactions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
