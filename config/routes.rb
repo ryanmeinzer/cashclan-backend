070c2ba2cafaa0ec2d45627be3d17a493a1bb3d2
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2022-03-08 22:12:26 -0800

    reverting all routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get '/members', to: "https://cashclan.com"
-  get '/transactions', to: "https://cashclan.com"
   resources :members
   resources :transactions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
