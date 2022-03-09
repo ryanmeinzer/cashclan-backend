9b5b80a11d5479fffb45c249cddae0381a31f9e3
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2022-03-08 22:05:34 -0800

    updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :members
-  resources :transactions
   direct :members do
     "https://cashclan.com"
   end
   direct :transactions do
     "https://cashclan.com"
   end
+  resources :members
+  resources :transactions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
