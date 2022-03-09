2c528d7af777a36efa0c6ed5b8729c1e7855f75b
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2022-03-08 22:02:06 -0800

    implementing redirects

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :members
   resources :transactions
-  direct '/members' do
+  direct :members do
+    "https://cashclan.com"
+  end
+  direct :transactions do
     "https://cashclan.com"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
