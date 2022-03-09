c0bfaea4bca45b45da7eb3fd34e100cad7842c5e
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2022-03-08 21:44:05 -0800

    testing cors origin update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -27,4 +27,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
+  allow do
+    origins 'https://cashclan-backend.herokuapp.com'
+
+    resource '*',
+      headers: :any,
+      methods: [:options]
+  end
 end
