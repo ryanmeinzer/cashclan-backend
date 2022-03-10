19f9c874619cff3aeec839b01ca776bd7d505f0c
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2022-03-10 02:38:27 -0800

    re-adding phone

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -56,7 +56,7 @@ class MembersController < ApplicationController
     def show
         member = Member.find(params[:id])
         render json: member.to_json({
-            except: [:created_at, :updated_at, :googleId, :email, :phone]
+            except: [:created_at, :updated_at, :googleId, :email]
         })
     end
 
