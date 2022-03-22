8021d294a21223a3384521e32fb2d1aa6d407254
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2022-03-21 22:07:56 -0700

    removed netlify from permitted cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,13 +14,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
   allow do
-    origins 'https://reverent-saha-216b51.netlify.app'
-
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
-  allow do
     origins 'https://cashclan.com'
 
     resource '*',
